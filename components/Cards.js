a1cefcf374cdd3c3854f4cb9f522876039c1ff30
DiamondDyer <diamond.l.dyer@gmail.com>
2020-07-10 13:55:47 -0400

    initial commit

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,60 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container')
+
+function ArticleMaker({title, url, authorName}){
+    const cardDiv = document.createElement('div');
+    const headline = document.createElement('div');
+    const author  = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const name = document.createElement('span');
+
+    cardDiv.className = 'card';
+    headline.className = 'headline';
+    author.className ='author';
+    imgContainer.className = 'img-container';
+
+    cardDiv.appendChild(headline);
+    cardDiv.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    author.appendChild(name);
+
+    cardDiv.addEventListener("click", (event)=> {
+        console.log(headline.textContent)
+    });
+
+headline.textContent = title;
+img.src = url;
+name.textContent = 'By ' + authorName;
+
+cardsContainer.appendChild(cardDiv)
+return cardDiv
+
+
+}
+
+
+
+const { default: Axios } = require("axios")
+
+
+const lambdaURL2 = 'https://lambda-times-backend.herokuapp.com/articles'
+console.log(Axios.get(lambdaURL2)).then(function (value) {
+    const articles = value.data.articles;
+    const headline = value.data.articles.headline
+    const authorName = value.data.articles.authorName
+    const img = value.data.articles.authorPhoto
+    
+    
+    articles.forEach(object => {
+      const tab = TabMaker(object)
+      
+    });
+  })
+  .catch(function (error) {
+    console.log(error)
+  });
--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header() {
+    
+
+    const headerDiv = document.createElement("div");
+    const date = document.createElement("span");
+    const h1 = document.createElement("h1");
+    const temperature = document.createElement("span");
+
+    headerDiv.className = 'header';
+    date.className = 'date';
+    temperature.className = 'temp';
+
+    date.textContent = 'MARCH 28, 2020';
+    h1.textContent = 'Lambda Times';
+    temperature.textContent = '98°';
+
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(h1);
+    headerDiv.appendChild(temperature);
+
+    return headerDiv;
+
+
+}
+
+const newHeader = Header();
+headerContainer.appendChild(newHeader);
